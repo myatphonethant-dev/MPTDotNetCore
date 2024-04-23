766a204a30d571dac879c140cee861fbed9a1407
Myat Phone Thant <myatphonethant.dev@gmail.com>
2024-04-23 22:44:52 +0630

    [OnProgress] Dapper

---
+++ MPTDotNetCore.ConsoleApp/Features/Dapper/DapperExample.cs
@@ -0,0 +1,41 @@
+﻿namespace MPTDotNetCore.ConsoleApp.Features.Dapper;
+
+public class DapperExample
+{
+    private string _connection;
+
+    public DapperExample(string connection)
+    {
+        _connection = connection;
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void Read()
+    {
+
+    }
+
+    public void Edit()
+    {
+
+    }
+
+    public void Create()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Delete()
+    {
+
+    }
+}
