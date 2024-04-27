0884a178da725bd9143418eb0d4472941f7f872d
Myat Phone Thant <myatphonethant.dev@gmail.com>
2024-04-27 15:58:20 +0630

    [InProgress] RestApi

---
+++ MPTDotNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MPTDotNetCore.RestApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult
+}
