e9200c2f01e12bfa2d990731b56727d6d82bc9ef
Myat Phone Thant <myatphonethant.dev@gmail.com>
2024-04-28 11:12:37 +0630

    -

--- MPTDotNetCore.RestApi/Controllers/BlogController.cs
+++ MPTDotNetCore.RestApi/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MPTDotNetCore.Shared.DbServices;
+using MPTDotNetCore.Shared.Models;
 
 namespace MPTDotNetCore.RestApi.Controllers;
 
@@ -6,6 +8,52 @@ namespace MPTDotNetCore.RestApi.Controllers;
 [ApiController]
 public class BlogController : ControllerBase
 {
+    private readonly AppDbContext _db;
+
+    public BlogController(AppDbContext db)
+    {
+        _db = db;
+    }
+
     [HttpGet]
-    public IActionResult
+    public IActionResult Read()
+    {
+        var lst = _db.TblBlogs.ToList();
+        return Ok(lst);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Edit(int id)
+    {
+        var item = _db.TblBlogs.FirstOrDefault(x => x.BlogId == id);
+
+        if (item == null) return NotFound("No Data Found.");
+        return Ok(item);
+    }
+
+    [HttpPost]
+    public IActionResult Create(BlogModel model)
+    {
+        _db.TblBlogs.Add(model);
+        _db.SaveChanges();
+        return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult Update()
+    {
+        return Ok();
+    }
+
+    [HttpPatch]
+    public IActionResult Patch()
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete()
+    {
+        return Ok();
+    }
 }
